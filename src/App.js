90cf599e99614cedb4312fef5f21c8268f83e6b6
ttran1017 <tylertran1017@gmail.com>
2022-03-26 07:25:15 -0600

    Made routing work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function App() {
 	return (
 		<div className="app">
 			<BrowserRouter>
-				<Route path="/" element={<Home />}></Route>
-				<Route path="/login" element={<Login />}></Route>
-				<Route path="/register" element={<Register />}></Route>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="login" element={<Login />}></Route>
+					<Route path="register" element={<Register />}></Route>
+				</Routes>
 			</BrowserRouter>
 		</div>
 	);
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,18 @@
 import React from "react";
 import "./Home.css";
 import Logo from "../../assets/logo_full.svg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
 	return (
-		<div className="home">
+		<section className="home">
 			<img className="home--logo" src={Logo} alt="" />
-			<input
-				type="button"
-				className="home--login home--button"
-				value={"login"}
-			/>
-			<input
-				type="button"
-				className="home--register home--button"
-				value={"register"}
-			/>
-		</div>
+			<Link to="/login" type="button" className="home--login home--button">
+				Login
+			</Link>
+			<Link to="/register" className="home--register home--button">
+				Register
+			</Link>
+		</section>
 	);
 }
