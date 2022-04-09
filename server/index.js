9c3c5f6a83afe2f987f2dd580f936277e92f9a56
Amanda Nguyen <77892730+AmandaNguye@users.noreply.github.com>
2022-04-08 23:39:12 -0600

    Add Initial Server, Routing, and DB Connection

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); //json middleware (parse json when sending to backend)
+
+app.use("/posts", require("./routes"));
+
+//listen on pc port
+const PORT = process.env.PORT || "5001";
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
